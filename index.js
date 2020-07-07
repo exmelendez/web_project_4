e2f7cbeb8ffe7ae4ef048f737b57c8f20c359b9e
Edgardo Melendez <exmelendez@gmail.com>
2020-07-07 18:16:32 -0400

    refactor: added js code to populate photo cards with object

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const titleInput = document.querySelector('.form__input_type_title');
 
 const toggleModal = () => {
   modal.classList.toggle('modal_is-open');
-  if(modal.classList.contains('modal_is-open')) {
+  if (modal.classList.contains('modal_is-open')) {
     nameInput.value = proNameField.textContent;
     titleInput.value = proTitleField.textContent;
   }
@@ -25,4 +25,59 @@ form.addEventListener('submit', (e) => {
   proTitleField.textContent = titleInput.value;
 
   toggleModal();
+});
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanois National Park",
+    link: "https://code.s3.yandex.net/web-code/vanois.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+const list = document.querySelector(".photos__grid");
+
+initialCards.forEach((data) => {
+  const cardTemplate = document.querySelector(".card-template").content.querySelector(".photos__item");
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".photos__image");
+  const cardTitle = cardElement.querySelector(".photos__title");
+  const cardLikeButton = cardElement.querySelector(".photos__love-btn");
+  const cardDeleteButton = cardElement.querySelector(".photos__delete-btn");
+
+  cardTitle.textContent = data.name;
+  cardImage.style.backgroundImage = `url(${data.link})`;
+
+  cardDeleteButton.addEventListener("click", () => {
+    //remove card
+  });
+
+  cardLikeButton.addEventListener("click", () => {
+    //ToggleCardState
+  });
+
+  cardImage.addEventListener("click", () => {
+    //open image modal
+  });
+
+  list.prepend(cardElement);
 });
