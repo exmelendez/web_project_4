1785fd387e9276f78429bb3532dbd10a6d6e5dae
Edgardo Melendez <exmelendez@gmail.com>
2020-09-20 09:23:45 -0400

    refactor: updated like number process

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,6 @@ class Card {
     this._id = cardData._id;
     this._ownerId = cardData.owner._id;
     this._likes = cardData.likes;
-    this._likesNumber = this._likes.length || 0;
     this._templateSelector = templateSelector;
     this._cardTemplate = document.querySelector(this._templateSelector).content.querySelector(".photos__item");
     this._handleCardClick = handleCardClick;
@@ -26,7 +25,6 @@ class Card {
     this._card = this._getCardTemplate().cloneNode(true);
     this._cardImage = this._card.querySelector(".photos__image");
     this._likesNumElement = this._card.querySelector(".photos__likes");
-    this._likesNumElement.textContent = this._likesNumber;
     this._cardImage.style.backgroundImage = `url(${this._link})`;
     this._card.querySelector(".photos__title").textContent = this._name;
     this._cardDeleteBtn = this._card.querySelector(".photos__delete-btn");
@@ -66,6 +64,8 @@ class Card {
     const heartFillClass = "photos__love-btn_liked";
 
     likedByOwner ? this._likeBtn.classList.add(heartFillClass) : this._likeBtn.classList.remove(heartFillClass);
+
+    this._setLikeNumber();
   }
 
   _getCardTemplate() {
@@ -78,6 +78,10 @@ class Card {
     });
 
     this._cardImage.addEventListener("click", () => this._handleCardClick(this._link, this._name));
+  }
+
+  _setLikeNumber() {
+    this._likesNumElement.textContent = this._likes.length;
   }
 }
 
